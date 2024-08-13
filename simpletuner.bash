55dda7a41d78fab445451f4873b252b4c8913e1d
Chris <chris.goringe@gmail.com>
2024-08-13 22:30:55 +1000

    st

---
+++ simpletuner.bash
@@ -0,0 +1,19 @@
+git clone --branch=release https://github.com/bghira/SimpleTuner.git
+cd SimpleTuner
+pip install -U poetry pip
+poetry install --no-root
+pip uninstall diffusers
+pip install git+https://github.com/huggingface/diffusers
+cp ../simple_tuner_files/* config/
+mv config/multidatabackend.json output/
+
+git config --global credential.helper store
+huggingface-cli login --add-to-git-credential --token $HF_READ
+
+apt -y install git-lfs
+mkdir -p datasets
+pushd datasets
+    git clone https://huggingface.co/datasets/ChrisGoringe/faces
+popd
+
+#bash train.sh
