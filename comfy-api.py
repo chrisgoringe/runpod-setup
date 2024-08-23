26ff78304729013a44f78dfdc82befe61fe5bba3
Chris <chris.goringe@gmail.com>
2024-08-23 14:03:27 +1000

    update

---
+++ comfy-api.py
@@ -0,0 +1,23 @@
+import json
+from urllib import request
+import random
+from functools import partial
+
+def queue_prompt(make_changes:list[callable]):
+    with open('workflow-api.json', 'r') as f: prompt = json.load(f)
+
+    for change in make_changes: change(prompt)
+
+    data = json.dumps({"prompt": prompt}).encode('utf-8')
+    req =  request.Request("http://127.0.0.1:8188/prompt", data=data)
+    request.urlopen(req)
+
+def set_all_seeds(theseed, prompt):
+    for node in prompt:
+        if 'inputs' in node:
+            for input in node['inputs']:
+                if input=='seed' or input=='noise_seed':
+                    prompt[node]['inputs'][input] = theseed
+
+if __name__=='__main__':
+    queue_prompt([partial(set_all_seeds, random.randint(0,1e9)),])
