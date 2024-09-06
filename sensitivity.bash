72b78bc83b91edd4602d5838392d4e3cd7b99003
Chris <chris.goringe@gmail.com>
2024-09-06 19:06:34 +1000

    update

--- sensitivity.bash
+++ sensitivity.bash
@@ -3,12 +3,10 @@ pip install -U "huggingface_hub[cli]"
 git config --global credential.helper store
 huggingface-cli login --token $HF_SAVE --add-to-git-credential
 model="https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/flux1-dev.safetensors"
-gguf="https://huggingface.co/city96/FLUX.1-dev-gguf/resolve/main/flux1-dev-Q6_K.gguf"
 
 pushd /workspace
     git clone http://github.com/comfyanonymous/ComfyUI
     pushd ComfyUI
-        wget --header "Authorization: Bearer $HF_READ" $gguf &
         pushd models
             pushd unet
                 wget --header "Authorization: Bearer $HF_READ" $model &
