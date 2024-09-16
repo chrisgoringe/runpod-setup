fef276308f74f785b4f32d4151eda513e417e753
Chris <chris.goringe@gmail.com>
2024-09-16 13:59:39 +1000

    compare

--- comfy-api.py
+++ comfy-api.py
@@ -1,27 +1,32 @@
 import json, time
 from urllib import request
-import random
+import os
+from tqdm import tqdm
 from functools import partial
 import requests
+from huggingface_hub import HfFileSystem
 
 def queue_prompt(jsonfile, make_changes:list[callable]):
-    with open(jsonfile, 'r') as f: prompt = json.load(f)
+    with open(jsonfile, 'r') as f: workflow = json.load(f)
 
-    for change in make_changes: change(prompt)
+    for change in make_changes: change(workflow)
 
-    data = json.dumps({"prompt": prompt}).encode('utf-8')
+    data = json.dumps({"prompt": workflow}).encode('utf-8')
     req =  request.Request("http://127.0.0.1:8188/prompt", data=data)
     request.urlopen(req)
 
-def set_all_seeds(theseed, prompt):
-    count = 0
-    for _,node in prompt.items():
-        if 'inputs' in node:
-            for input in node['inputs']:
-                if input=='seed' or input=='noise_seed':
-                    node['inputs'][input] = theseed
-                    count += 1
-    print(f"set {count} seeds to {theseed}")
+def upload(local_filepath, names):
+    basename = os.path.basename(local_filepath)
+    index = int(basename[8:13]) - 1
+    name = names[index]
+    remote_filepath = f"ChrisGoringe/MixedQuantFlux/examples/{name}.png"
+
+    hf = HfFileSystem()
+    hf.put_file(lpath=local_filepath, rpath=remote_filepath)
+
+def push_outputs(directory):
+    for f in os.listdir(directory):
+        upload(os.path.join(directory,f))
 
 def wait_for_ready():
     while True:
@@ -35,34 +40,34 @@ def wait_for_done():
     def inq():
         a = requests.get("http://127.0.0.1:8188/queue").json()
         total_queue = len(a['queue_running']) + len(a['queue_pending'])
-        try: total_queue += int( requests.get("http://127.0.0.1:8188/upload_queue").json()['upload_queue'] )
-        except: pass
         return total_queue
     
     while inq():
         time.sleep(10)
-        while inq(): 
+        while (x:=inq()):
+            print(f"\r{x} in queue   ", end='') 
             time.sleep(10)
-        time.sleep(10)
 
-def download_internals():
-    a = requests.get("http://127.0.0.1:8188/download_internals").json()
-    print(a)
 
-def upload_internals():
-    a = requests.get("http://127.0.0.1:8188/upload_internals").json()
-    print(a)   
+def set_prompt(prompt, workflow): workflow["203"]["inputs"]["string"] = prompt
 
-fi2=False
+def set_model(model, workflow): workflow["194"]["inputs"]["unet_name"] = model
 
 if __name__=='__main__':
     wait_for_ready()
-    if fi2:
-        jsonfile = 'workflow_api_fi2.json'
-        n = 100
-    else:
-        jsonfile = 'workflow_api.json'
-        n = 11
-    for _ in range(n): queue_prompt(jsonfile=jsonfile, make_changes=[partial(set_all_seeds, random.randint(0,1e9)),])
+
+    jsonfile = 'workflow_gguf.json'
+    models = ['flux1-dev_mx5_1.gguf', 'flux1-dev_mx5_9.gguf', 'flux1-dev_mx6_9.gguf', 'flux1-dev_mx7_4.gguf', 
+              'flux1-dev_mx7_6.gguf', 'flux1-dev_mx8_4.gguf', 'flux1-dev_mx9_2.gguf', 'flux1-dev_mx9_6.gguf', ]
+    
+    with open('prompts.txt', 'r') as f: prompts = f.readlines()
+
+    names = []
+    for model in models:
+        for i, prompt in enumerate(prompts):
+            queue_prompt(jsonfile=jsonfile, make_changes=[partial(set_prompt, prompt), partial(set_model, model)])
+            names.append( f"model_{model.split('.')[0][-3:]}-prompt_{i}" )
     wait_for_done()
-    time.sleep(10)
+
+    push_outputs('output', names)
+
--- flux-auto-comfy.bash
+++ flux-auto-comfy.bash
@@ -1,14 +1,20 @@
 pip install -U "huggingface_hub[cli]"
 git config --global credential.helper store
 huggingface-cli login --token $HF_SAVE --add-to-git-credential
-model="https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/flux1-dev.safetensors"
 
 pushd /workspace
     git clone http://github.com/comfyanonymous/ComfyUI
     pushd ComfyUI
         pushd models
             pushd unet
-                wget --header "Authorization: Bearer $HF_READ" $model &
+                wget --header "Authorization: Bearer $HF_READ" https://huggingface.co/ChrisGoringe/MixedQuantFlux/resolve/main/flux1-dev_mx5_1.gguf &
+                wget --header "Authorization: Bearer $HF_READ" https://huggingface.co/ChrisGoringe/MixedQuantFlux/resolve/main/flux1-dev_mx5_9.gguf &
+                wget --header "Authorization: Bearer $HF_READ" https://huggingface.co/ChrisGoringe/MixedQuantFlux/resolve/main/flux1-dev_mx6_9.gguf &
+                wget --header "Authorization: Bearer $HF_READ" https://huggingface.co/ChrisGoringe/MixedQuantFlux/resolve/main/flux1-dev_mx7_4.gguf &
+                wget --header "Authorization: Bearer $HF_READ" https://huggingface.co/ChrisGoringe/MixedQuantFlux/resolve/main/flux1-dev_mx7_6.gguf &
+                wget --header "Authorization: Bearer $HF_READ" https://huggingface.co/ChrisGoringe/MixedQuantFlux/resolve/main/flux1-dev_mx8_4.gguf &
+                wget --header "Authorization: Bearer $HF_READ" https://huggingface.co/ChrisGoringe/MixedQuantFlux/resolve/main/flux1-dev_mx9_2.gguf &
+                wget --header "Authorization: Bearer $HF_READ" https://huggingface.co/ChrisGoringe/MixedQuantFlux/resolve/main/flux1-dev_mx9_6.gguf &
             popd
             pushd clip
                 wget https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp16.safetensors &
@@ -20,11 +26,13 @@ pushd /workspace
         popd
         pip install -r requirements.txt
         pushd custom_nodes
-            git clone https://github.com/chrisgoringe/flux-poke
+            git clone https://github.com/city96/ComfyUI-GGUF
             pushd flux-poke
                 pip install -r requirements.txt
             popd
+            git clone https://github.com/chrisgoringe/cg-quicknodes
         popd
+        pip install huggingface_hub
     popd
 popd
 
@@ -35,7 +43,7 @@ popd
 
 python comfy-api.py
 
-runpodctl remove pod $RUNPOD_POD_ID
+#runpodctl remove pod $RUNPOD_POD_ID
 
 
 
